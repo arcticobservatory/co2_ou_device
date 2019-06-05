f6cd5c6658f568c802e32fb958ec3fcf58c48205
Mike Murphy <michael.j.murphy@uit.no>
2019-06-05 14:17:19 +0200

    Refactor to Co2Unit object

--- src/main.py
+++ src/main.py
@@ -12,68 +12,72 @@ from machine import Pin
 from machine import SD
 from ds3231 import DS3231
 
+class Co2Unit(object):
 
-# Init external RTC
-# # https://docs.pycom.io/firmwareapi/pycom/machine/i2c.html
-ertc = DS3231(0, pins=('P22','P21'))
+    def __init__(self):
+        self.init_sensors()
 
-# Init external SD card
-# # https://docs.pycom.io/firmwareapi/pycom/machine/spi.html
-SD_CS = Pin('P12')
-spi = SPI(0, mode=SPI.MASTER)
+    def init_sensors(self):
 
-sd = sdcard.SDCard(spi, SD_CS)
-os.mount(sd, '/sd2')
-print(os.listdir('/sd2'))
+        # Init external RTC
+        # # https://docs.pycom.io/firmwareapi/pycom/machine/i2c.html
+        self.ertc = DS3231(0, pins=('P22','P21'))
 
-# Init external temperature sensor
-# https://docs.pycom.io/tutorials/all/owd.html
-ow = OneWire(Pin('P3'))
-ext_temp = DS18X20(ow)
+        # Init external SD card
+        # # https://docs.pycom.io/firmwareapi/pycom/machine/spi.html
+        self.spi = SPI(0, mode=SPI.MASTER)
+        SD_CS = Pin('P12')
+        self.sd = sdcard.SDCard(self.spi, SD_CS)
+        os.mount(self.sd, '/sd2')
+        print(os.listdir('/sd2'))
 
-# Init flash detector diode
-# # https://docs.pycom.io/firmwareapi/pycom/machine/pin.html
+        # Init external temperature sensor
+        # https://docs.pycom.io/tutorials/all/owd.html
+        ow = OneWire(Pin('P3'))
+        self.ext_temp = DS18X20(ow)
 
-def pin_handler(arg):
-    print("pin change")
+        # Init flash detector diode
+        # # https://docs.pycom.io/firmwareapi/pycom/machine/pin.html
+        self.flash_pin = Pin('P4', mode=Pin.IN, pull=Pin.PULL_UP)
+        self.flash_pin.callback(Pin.IRQ_FALLING, self.on_flash_pin)
 
-flash_pin = Pin('P4', mode=Pin.IN, pull=Pin.PULL_UP)
-flash_pin.callback(Pin.IRQ_FALLING, pin_handler)
+    def on_flash_pin(self, arg):
+        print("pin change")
 
+    def take_reading(self):
 
-def take_reading():
+        # Start temperature reading
+        self.ext_temp.start_conversion()
+        ext_t_start_ticks = time.ticks_ms()
 
-    # Start temperature reading
-    ext_temp.start_conversion()
-    ext_t_start_ticks = time.ticks_ms()
+        # Wait for temperature reading
+        # Temperature reading seems to take around 650 ms (datasheet says under 750 ms)
+        while True:
+            ext_t_reading = self.ext_temp.read_temp_async()
+            if ext_t_reading != None:
+                ext_t_ticks = time.ticks_diff(ext_t_start_ticks, time.ticks_ms())
+                break
+            time.sleep_ms(1)
 
-    # Wait for temperature reading
-    # Temperature reading seems to take around 650 ms (datasheet says under 750 ms)
-    while True:
-        ext_t_reading = ext_temp.read_temp_async()
-        if ext_t_reading != None:
-            ext_t_ticks = time.ticks_diff(ext_t_start_ticks, time.ticks_ms())
-            break
-        time.sleep_ms(1)
+        # Read flash sensor
+        flash_reading = self.flash_pin()
 
+        # Read RTC
+        wtime = self.ertc.get_time(True)
+        wtime_raw = time.mktime(wtime)
 
-    # Read flash sensor
-    flash_reading = flash_pin()
+        return {
+                "wtime": wtime,
+                "wtime_raw": wtime_raw,
+                "ext_t": ext_t_reading,
+                "ext_t_ms": ext_t_ticks,
+                "flash": flash_reading,
+                }
 
-    # Read RTC
-    wtime = ertc.get_time(True)
-    wtime_raw = time.mktime(wtime)
-
-    return {
-            "wtime": wtime,
-            "wtime_raw": wtime_raw,
-            "ext_t": ext_t_reading,
-            "ext_t_ms": ext_t_ticks,
-            "flash": flash_reading,
-            }
+co2unit = Co2Unit()
 
 while True:
 
-    reading = take_reading()
+    reading = co2unit.take_reading()
     print(reading)
     time.sleep(5)
