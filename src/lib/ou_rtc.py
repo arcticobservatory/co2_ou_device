bb0b40cf0779d7575c080821ff8a4e463fc18eda
Mike Murphy <michael.j.murphy@uit.no>
2019-06-06 17:36:29 +0200

    Break RTC out into lib

---
+++ src/lib/ou_rtc.py
@@ -0,0 +1,32 @@
+from machine import RTC
+
+from ds3231 import DS3231
+import logging
+
+_logger = logging.getLogger("rtc")
+
+def time_reasonable(time_tuple):
+    return time_tuple[0] >= 2019
+
+class OuRtc(object):
+
+    def __init__(self):
+        _logger.info("Initializing RTC")
+        self.irtc = RTC()
+        self.ertc = DS3231(0, pins=('P22','P21'))
+
+        itime = self.irtc.now()
+        etime = self.ertc.get_time()
+
+        if not time_reasonable(itime):
+            _logger.warning("Internal RTC not set: %s", itime)
+        else:
+            _logger.info("Internal RTC time: %s", itime)
+
+        if not time_reasonable(etime):
+            _logger.warning("External RTC not set: %s", etime)
+        else:
+            _logger.info("External RTC time: %s", etime)
+
+    def get_time(self, set_internal_rtc=False):
+        return self.ertc.get_time(set_rtc=set_internal_rtc)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 import time
 
-from ds3231 import DS3231
 import logging
 
-from ou_storage import OuStorage
+from ou_rtc import OuRtc
 from ou_sensors import OuSensors
+from ou_storage import OuStorage
 
 logging.basicConfig(level=logging.DEBUG)
 
-ertc = DS3231(0, pins=('P22','P21'))
+ou_rtc = OuRtc()
 ou_sensors = OuSensors()
 ou_storage = OuStorage()
 
 while True:
 
     print()
-    rtime = ertc.get_time(True)
+    rtime = ou_rtc.get_time()
     reading = ou_sensors.take_reading()
     reading["rtime"] = rtime
     ou_storage.record_reading(reading)
