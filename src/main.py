93d8401c7a0d156a7f369fcfdb677b570f13cc1b
Mike Murphy <michael.j.murphy@uit.no>
2019-06-06 18:09:20 +0200

    Put test loop into function

--- src/main.py
+++ src/main.py
@@ -12,14 +12,18 @@ ou_rtc = OuRtc()
 ou_sensors = OuSensors()
 ou_storage = OuStorage()
 
-while True:
+def simple_read_loop():
 
-    rtime = ou_rtc.get_time()
-    reading = ou_sensors.take_reading()
-    reading["rtime"] = rtime
-    (path, row) = ou_storage.record_reading(reading)
-    logging.info("%s: %s\t| raw reading: %s", path, row, reading)
+    while True:
 
-    logging.debug("Going into light sleep")
-    time.sleep(5)
-    logging.debug("Woke up")
+        rtime = ou_rtc.get_time()
+        reading = ou_sensors.take_reading()
+        reading["rtime"] = rtime
+        (path, row) = ou_storage.record_reading(reading)
+        logging.info("%s: %s\t| raw reading: %s", path, row, reading)
+
+        logging.debug("Going into light sleep")
+        time.sleep(5)
+        logging.debug("Woke up")
+
+simple_read_loop()
