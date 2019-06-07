dcdf5ade325275581c65a92f5804da5093c670ad
Mike Murphy <michael.j.murphy@uit.no>
2019-06-07 14:00:04 +0200

    Fix swapped timeout/sleep message

--- src/lib/polling.py
+++ src/lib/polling.py
@@ -26,7 +26,7 @@ def poll_sleep_loop(poll_fns_dict, ticks_unit="ms", timeout_ticks=1000, sleep_ti
     results = {k: None for k, _ in poll_fns_dict.items()}
 
     _logger.debug("Entering poll loop for %s", poll_fns_dict.keys())
-    _logger.debug("Each sleep: %d %s, timeout after: %d %s",
+    _logger.debug("Timeout after %d %s, each sleep %d %s",
                 timeout_ticks, ticks_unit, sleep_ticks, ticks_unit)
     while True:
         for k, fn in poll_fns_dict.items():
