c7ecbb6df16c9becac5c333d30270f01a211d67e
Mike Murphy <michael.j.murphy@uit.no>
2019-06-21 18:05:26 +0200

    Remove incomplete update code

--- src/main.py
+++ src/main.py
@@ -26,25 +26,3 @@ post._logger.setLevel(logging.DEBUG)
 post.display_errors_led()
 
 print("Failure flags: {:#018b}".format(post.failures))
-
-# Updates
-import co2unit_update
-co2unit_update._logger.setLevel(logging.DEBUG)
-import os
-
-co2unit_hw._logger.setLevel(logging.DEBUG)
-
-os.mount(hw.sdcard(), "/sd")
-os.chdir("/sd/updates/update-2019-06-13")
-appimg = 'firmware-pycom-fipy-1.18.2.r7/appimg.bin'
-print()
-print("To flash Pycom firmware run:")
-print("co2unit_update.do_pycom_ota(appimg)")
-print()
-
-dup = "firmware-modem-CATM1-41065/CATM1-41065/CATM1-41065.dup"
-elf = "firmware-modem-CATM1-41065/CATM1-41065/updater.elf"
-print()
-print("To update modem firmware run:")
-print("co2unit_update.do_modem_upgrade(dup, elf)")
-print()
